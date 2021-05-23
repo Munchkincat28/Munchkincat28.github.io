98390171c049adcdd7644a034fea078c72f090ee
Munchincate28 <you@example.com>
2021-05-23 21:38:18 +0700

    Munchkincat

---
+++ src/views/Home/components/EarnAPRCard.tsx
@@ -0,0 +1,79 @@
+import React, { useMemo } from 'react'
+import styled from 'styled-components'
+import { Heading, Card, CardBody, Flex, ArrowForwardIcon, Skeleton } from '@pancakeswap/uikit'
+import max from 'lodash/max'
+import { NavLink } from 'react-router-dom'
+import { useTranslation } from 'contexts/Localization'
+import BigNumber from 'bignumber.js'
+import { getFarmApr } from 'utils/apr'
+import { useFarms, usePriceCakeBusd, useGetApiPrices } from 'state/hooks'
+import { getAddress } from 'utils/addressHelpers'
+
+const StyledFarmStakingCard = styled(Card)`
+  margin-left: auto;
+  margin-right: auto;
+  width: 100%;
+
+  ${({ theme }) => theme.mediaQueries.lg} {
+    margin: 0;
+    max-width: none;
+  }
+
+  transition: opacity 200ms;
+  &:hover {
+    opacity: 0.65;
+  }
+`
+const CardMidContent = styled(Heading).attrs({ scale: 'xl' })`
+  line-height: 44px;
+`
+const EarnAPRCard = () => {
+  const { t } = useTranslation()
+  const { data: farmsLP } = useFarms()
+  const prices = useGetApiPrices()
+  const cakePrice = usePriceCakeBusd()
+
+  const highestApr = useMemo(() => {
+    const aprs = farmsLP
+      // Filter inactive farms, because their theoretical APR is super high. In practice, it's 0.
+      .filter((farm) => farm.pid !== 0 && farm.multiplier !== '0X')
+      .map((farm) => {
+        if (farm.lpTotalInQuoteToken && prices) {
+          const quoteTokenPriceUsd = prices[getAddress(farm.quoteToken.address).toLowerCase()]
+          const totalLiquidity = new BigNumber(farm.lpTotalInQuoteToken).times(quoteTokenPriceUsd)
+          return getFarmApr(farm.poolWeight, cakePrice, totalLiquidity)
+        }
+        return null
+      })
+
+    const maxApr = max(aprs)
+    return maxApr?.toLocaleString('en-US', { maximumFractionDigits: 2 })
+  }, [cakePrice, farmsLP, prices])
+
+  const aprText = highestApr || '-'
+  const earnAprText = t('Earn up to %highestApr% APR in Farms', { highestApr: aprText })
+  const [earnUpTo, InFarms] = earnAprText.split(aprText)
+
+  return (
+    <StyledFarmStakingCard>
+      <NavLink exact activeClassName="active" to="/farms" id="farm-apr-cta">
+        <CardBody>
+          <Heading color="contrast" scale="lg">
+            {earnUpTo}
+          </Heading>
+          <CardMidContent color="#7645d9">
+            {highestApr ? `${highestApr}%` : <Skeleton animation="pulse" variant="rect" height="44px" />}
+          </CardMidContent>
+          <Flex justifyContent="space-between">
+            <Heading color="contrast" scale="lg">
+              {InFarms}
+            </Heading>
+            <ArrowForwardIcon mt={30} color="primary" />
+          </Flex>
+        </CardBody>
+      </NavLink>
+    </StyledFarmStakingCard>
+  )
+}
+
+export default EarnAPRCard
